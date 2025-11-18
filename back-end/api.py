0f90bd6c87384d613645f8a520357876ac572405
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-11-18 23:21:09 +0100

    Add the first GET endpoint to retrieve files in json

--- back-end/api.py
+++ back-end/api.py
@@ -1,7 +1,26 @@
 from flask import Flask
+import os
+import time
 
-app = Flask(__name__)
+PATH='/Users/rithchea/Desktop/Life/Personal Project/FileDownloader/back-end/files'
+
+app = Flask('back-end')
 
 @app.route("/")
 def hello_world():
-    return '<p>Hello, World !</p>'
+    return '<p>Hello, World!</p>'
+
+@app.get('/api/files')
+def get_files():
+    filename_list = []
+    for root, dirnames, filenames in os.walk(PATH):
+        for file_name in filenames:
+            modification_time = os.path.getmtime(f"./files/{file_name}")
+            readable_time = time.ctime(modification_time)
+            file_size = os.path.getsize(f"./files/{file_name}")
+            filename_list.append({
+                "name": file_name,
+                "size": f"{file_size} bytes",
+                "last modified": readable_time
+            })
+    return filename_list

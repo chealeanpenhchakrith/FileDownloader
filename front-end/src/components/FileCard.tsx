58d5cf676d19122d6d7c520ad05638f6fbed2ab1
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-11-20 19:46:11 +0100

    Add a function to match files with their respective visual icons

--- front-end/src/components/FileCard.tsx
+++ front-end/src/components/FileCard.tsx
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Png from "../assets/icons/Png.png";
+import Json from "../assets/icons/Json.png";
+import Txt from "../assets/icons/Txt.png";
+import Pdf from "../assets/icons/Pdf.png";
 
 export default function FileCard() {
   const [list, setList] = useState<File[]>([]);
@@ -22,14 +26,33 @@ export default function FileCard() {
 
   useEffect(() => {
     fetchFiles();
+    matchFormat("notes.txt");
   }, []);
 
+  function matchFormat(fileName: string) {
+    const separatedList: string = fileName.split(".")[1];
+    switch (separatedList) {
+      case "txt":
+        return <img src={Txt} alt="txt" width={50} height={50} />;
+        break;
+      case "pdf":
+        return <img src={Pdf} alt="pdf" width={50} height={50} />;
+        break;
+      case "png":
+        return <img src={Png} alt="png" width={50} height={50} />;
+        break;
+      case "json":
+        return <img src={Json} alt="json" width={50} height={50} />;
+        break;
+    }
+  }
   return (
     <>
-      <ul className="list-disc list-inside">
+      <ul>
         {list.map((file: File) => {
           return (
-            <li key={file.name}>
+            <li key={file.name} className="flex mt-10 items-center">
+              {matchFormat(file.name)}
               {file.name}, {file.size}, {file.last_modified}
             </li>
           );
